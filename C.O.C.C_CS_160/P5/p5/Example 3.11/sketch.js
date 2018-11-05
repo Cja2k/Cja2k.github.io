214c8215388a7df45f1e96357a62b63dc36c7cee
Cody <codyjonathanallen@gmail.com>
2018-11-05 00:39:13 -0800

    I'm Tired, I'll work on fixing the book questions tomorrow morning sorry

---
+++ C.O.C.C_CS_160/P5/p5/Example 3.11/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+createCanvas(600, 400);
+}
+function draw() {
+background(204);
+rect(160, 30, 260, 20);
+// The ellipse draws on top of the rectangle
+// because it comes after in the code
+ellipse(140, 0, 190, 190);
+}
